7b321e249f70792d60764a64acfdaed3b644ede4
carthioli <carthivie@gmail.com>
2021-10-25 18:10:34 -0300

    funções e pasta
    criada e testadas as funções dos números e operadores, e alterado nome da pasta js.

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+function numero(){
+  alert('numero')
+}
+function multiplicar(){
+  alert('multiplicar')
+}
+function somar(){
+  alert('somar')
+}
+function dividir(){
+  alert('dividir')
+}
+function subtrair(){
+  alert('subtrair')
+}
+function igual(){
+  alert('igual')
+}
+function limpar(){
+  alert('limpar')
+}
+function virgula(){
+  alert('virgula')
+}
+function negativo(){
+  alert('negativo')
+}
