2c62091d11e0eeb7fed58c9b4efcc841106eb392
carthioli <carthivie@gmail.com>
2021-10-26 10:47:56 -0300

    adicionado valores ao visor e especificado botões operadores
    Botões operadores especificados por id, div resultado recebendo os números e operadores(soma, multiplicar, subtrair e dividir) quando clicado

--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,50 @@
 primeirovalor = 0
 segundovalor  = 0
 result = 0
+div = 0
+
 
 function resultado(){
-
+  div = document.getElementById("resultado")
+  div.innerHTML = 100
+  
+  
 }
 function numero(clicked_id){
-  alert(clicked_id)
+  num = clicked_id
+  div = document.getElementById("resultado")
+  div.innerHTML += num
+  
 }
-function multiplicar(){
-  alert('multiplicar')
+function multiplicar(clicked_id){
+  multiplica = clicked_id
+  div = document.getElementById("resultado")
+  div.innerHTML += multiplica
 }
-function somar(){
-  alert('somar')
+function somar(clicked_id){
+  soma = clicked_id
+  div = document.getElementById("resultado")
+  div.innerHTML += soma
 }
-function dividir(){
-  alert('dividir')
+function dividir(clicked_id){
+  divide = clicked_id
+  div = document.getElementById("resultado")
+  div.innerHTML += divide
 }
-function subtrair(){
-  alert('subtrair')
+function subtrair(clicked_id){
+  subtrai = clicked_id
+  div = document.getElementById("resultado")
+  div.innerHTML += subtrai
 }
 function igual(){
-  alert('igual')
+  this.resultado()
 }
 function limpar(){
-  alert('limpar')
+  div.innerHTML = null
 }
 function virgula(){
-  alert('virgula')
+  
 }
 function negativo(){
-  alert('negativo')
+  
 }
