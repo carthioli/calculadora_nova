4bfd7daf708031449e5253bfadf9a704bc3e8ac1
carthioli <carthivie@gmail.com>
2021-10-27 11:48:58 -0300

    Finalizada funções e operações
    verifica se a divisão é por 0 ok
    primeiro e segundo numero verificando se a divisão não é por 0.

--- javascript/script.js
+++ javascript/script.js
@@ -8,26 +8,20 @@ operacao = false
  dividi = false
  subtrai = false
 
-
-
 function resultado(clicked_id){
   div = document.getElementById("resultado")
    
   if ( multi == true ) {
-
     this.multiplicar(clicked_id)
     div.innerHTML = result
-    
     multi = false
     primeirovalor = result
     segundovalor = ""
- 
   } 
   else{
     if ( soma == true ) {
       this.somar(clicked_id)
       div.innerHTML = result
-     
       soma = false
       primeirovalor = result
       segundovalor = ""
@@ -36,7 +30,6 @@ function resultado(clicked_id){
   if ( dividi == true ) {
     this.dividir(clicked_id)
     div.innerHTML = result
-    
     dividi = false
     primeirovalor = result
     segundovalor = ""
@@ -45,60 +38,43 @@ function resultado(clicked_id){
     if ( subtrai == true ) {
     this.subtrair(clicked_id)
     div.innerHTML = result
-    
     subtrai = false
     primeirovalor = result
     segundovalor = ""
     }
   }
-  }
-  
+}
 function numero(clicked_id){
   num = clicked_id
-  
   div = document.getElementById("resultado")
   div.innerHTML += num
-
   if ( operacao == true ){
     segundovalor += num 
-     
   }
   else{
     primeirovalor += num
-  }
-
-    
+  } 
 }
 function multiplicar(clicked_id){
-  
   div = document.getElementById("resultado")
- 
-  
+   
   operacao = true
   multi = true
   
   if ( operacao == true ) {
-    multi = false
-    
+    multi = false 
     this.resultado()
-    
     div.innerHTML += clicked_id 
     multi = true
-    
   }
-  
   result = primeirovalor * segundovalor
-  if ( segundovalor != "" ){
-    
+  if ( segundovalor != "" ){ 
     div.innerHTML = result + clicked_id
     primeirovalor = result
     segundovalor = ""
   }
-  
-  
 }
 function somar(clicked_id){
-
   div = document.getElementById("resultado")
   
   operacao = true
@@ -106,14 +82,10 @@ function somar(clicked_id){
   
   if ( operacao == true ) {
     soma = false
-    
     this.resultado()
-    
     div.innerHTML += clicked_id 
     soma = true
-    
-  }
-  
+  }  
   result = parseFloat(primeirovalor) + parseFloat(segundovalor)
 
   if ( segundovalor != "" ){
@@ -133,26 +105,40 @@ function dividir(clicked_id){
   
   if ( operacao == true ) {
     dividi = false
-    
     this.resultado()
-    
     div.innerHTML += clicked_id 
-    dividi = true
-    
+    dividi = true   
   }
-
   result = primeirovalor / segundovalor
 
   if ( segundovalor != "" ){
-    
     div.innerHTML = result + clicked_id
     primeirovalor = result
+    
+
+    if( segundovalor == 0  ) {
+      primeirovalor = ""
+      segundovalor = ""
+      result = ""
+      alert("Não há divisão em base zero!")
+     
+    }
     segundovalor = ""
   }
- 
+  if ( primeirovalor == 0 ) {
+    
+    dividi = false
+    this.limpar()
+    alert("Não há divisão em base zero!")
+  }
+
+  console.log(primeirovalor)
+  console.log(segundovalor)
+  console.log(num)
+  console.log(operacao)
+
 }
 function subtrair(clicked_id){
-  
   div = document.getElementById("resultado")
   
   operacao = true
@@ -160,27 +146,20 @@ function subtrair(clicked_id){
 
   if ( operacao == true ) {
     subtrai = false
-    
     this.resultado()
-    
     div.innerHTML += clicked_id 
     subtrai = true
-    
   }
   result = primeirovalor - segundovalor
 
   if ( segundovalor != "" ){
-    
     div.innerHTML = result + clicked_id
     primeirovalor = result
     segundovalor = ""
-   
   }
-
 }
 function igual(){
   this.resultado()
-  
 }
 function limpar(){
   div.innerHTML = null
@@ -188,4 +167,3 @@ function limpar(){
   primeirovalor = ""
   segundovalor = ""
 }
-
