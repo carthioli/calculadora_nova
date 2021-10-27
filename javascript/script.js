b5ecebbc4af72cdf46b09a59086d38a3d8a2acf5
carthioli <carthivie@gmail.com>
2021-10-27 17:54:26 -0300

    alterada função historico

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,21 @@
-primeirovalor = ""
-segundovalor  = ""
-
-operacao = false
+/*ARMAZENA OS VALORES*/
+  result = ""
+  primeirovalor = ""
+  segundovalor  = ""
+/*VERICA ESTADO DA OPERAÇÃO*/
+  operacao = false
 /*OPERADORES*/
- soma = false
- multi = false
- dividi = false
- subtrai = false
-/*VERIFICAÇÃO HISTORICO*/
- hsoma = ""
- hmulti = ""
- hdividi = ""
- hsubtrai = ""
-
-
+  soma = false
+  multi = false
+  dividi = false
+  subtrai = false
+/*HISTORICO*/
+  hresult = ""
+  hprimeirovalor = ""
+  hsegundovalor  = ""
+/*VERIFICAÇÃO DA OPERAÇÃO HISTORICO*/
+  resultope = ""
+/*DEFINE QUAL OPERAÇÃO ESTÁ SENDO REALIZADA*/
 function resultado(clicked_id){
   div = document.getElementById("resultado")
    
@@ -24,14 +26,12 @@ function resultado(clicked_id){
     primeirovalor = result
     segundovalor = ""
   } 
-  else{
-    if ( soma == true ) {
-      this.somar(clicked_id)
-      div.innerHTML = result
-      soma = false
-      primeirovalor = result
-      segundovalor = ""
-    }
+  if ( soma == true ) {
+    this.somar(clicked_id)
+    div.innerHTML = result
+    soma = false
+    primeirovalor = result
+    segundovalor = ""
   }
   if ( dividi == true ) {
     this.dividir(clicked_id)
@@ -40,37 +40,38 @@ function resultado(clicked_id){
     primeirovalor = result
     segundovalor = ""
   } 
-  else {
-    if ( subtrai == true ) {
+  if ( subtrai == true ) {
     this.subtrair(clicked_id)
     div.innerHTML = result
     subtrai = false
     primeirovalor = result
     segundovalor = ""
-    }
   }
 }
+/*DEFINE EM QUAL VARIAVEL O NÚMERO ESTÁ GUARDADO*/
 function numero(clicked_id){
   num = clicked_id
   div = document.getElementById("resultado")
   div.innerHTML += num
   if ( operacao == true ){
     segundovalor += num
+    this.armazena()
   }
   else{
     primeirovalor += num
+    this.armazena()
   } 
 }
+/*REALIZA OPERAÇÃO MULTIPLICAR*/
 function multiplicar(clicked_id){
-  div = document.getElementById("resultado")
-   
+  div = document.getElementById("resultado") 
   operacao = true
   multi = true
-  
+  this.armazena()
   if ( operacao == true ) {
     multi = false 
     this.resultado()
-    div.innerHTML += clicked_id 
+    div.innerHTML += clicked_id
     multi = true
   }
   result = primeirovalor * segundovalor
@@ -81,9 +82,10 @@ function multiplicar(clicked_id){
     this.armazena()
   }
 }
+/*REALIZA OPERAÇÃO SOMAR*/
 function somar(clicked_id){
+  this.armazena()
   div = document.getElementById("resultado")
-  
   operacao = true
   soma = true
   
@@ -96,14 +98,13 @@ function somar(clicked_id){
   result = parseFloat(primeirovalor) + parseFloat(segundovalor)
 
   if ( segundovalor != "" ){
-    
     div.innerHTML = result + clicked_id
     primeirovalor = result
     segundovalor = ""
     this.armazena()
   }
-
 }
+/*REALIZA OPERAÇÃO DIVIDIR*/
 function dividir(clicked_id){
   
   div = document.getElementById("resultado")
@@ -122,46 +123,35 @@ function dividir(clicked_id){
   if ( segundovalor != "" ){
     div.innerHTML = result + clicked_id
     primeirovalor = result
-    
 
     if( segundovalor == 0  ) {
       primeirovalor = ""
       segundovalor = ""
       result = ""
-      alert("Não há divisão em base zero!")
-     
+      alert("Não há divisão em base zero!") 
     }
-    segundovalor = ""
-    this.armazena()
-    
+  segundovalor = ""
+  this.armazena()
   }
-  if ( primeirovalor == 0 ) {
-    
+  if ( primeirovalor == 0 ) { 
     dividi = false
     this.limpar()
     alert("Não há divisão em base zero!")
   }
-
-  console.log(primeirovalor)
-  console.log(segundovalor)
-  console.log(num)
-  console.log(operacao)
-
 }
+/*REALIZA OPERAÇÃO SUBTRAIR*/
 function subtrair(clicked_id){
   div = document.getElementById("resultado")
-  
   operacao = true
   subtrai = true
-
+  this.armazena()
   if ( operacao == true ) {
     subtrai = false
     this.resultado()
-   div.innerHTML += clicked_id 
+    div.innerHTML += clicked_id 
     subtrai = true
   }
   result = primeirovalor - segundovalor
-
   if ( segundovalor != "" ){
     div.innerHTML = result + clicked_id
     primeirovalor = result
@@ -169,30 +159,59 @@ function subtrair(clicked_id){
     this.armazena()
   }
 }
+/*CHAMA O RESLTADO*/
 function igual(){
   this.resultado()
 }
+/*ZERA OPERAÇÕES E LIMPA O HISTÓRICO*/
 function limpar(){
-  div.innerHTML = null
+  div.innerHTML = ""
   operacao = false
   primeirovalor = ""
   segundovalor = ""
+  historico.innerHTML = ""
+  hprimeirovalor = ""
+  hsegundovalor = ""
+  hresult = ""
 }
+/*ARMAZENA O HISTÓRICO*/
 function armazena(){
   historico = document.getElementById("historico")
-  historico.innerHTML += result + "<br>"
-  
-  /*if ( soma == true ) {
-    hsoma = "+"
+  if ( soma == true ) {
+    resultope = "+"
   }
   if ( multi == true ) {
-    hmulti = "&times"
+    resultope = "&times"
   }
   if ( subtrai == true ) {
-    hsubtrai = "-"
+    resultope = "-"
   }
   if ( dividi == true) {
-    hdividi = "&divide"
+    resultope = "&divide"
   }
-  console.log(hsoma)*/
+  if ( operacao == true ){
+    hsegundovalor  = num
+  }
+ else {
+    hprimeirovalor = num
+  } 
+  if ( result != "" ) {
+  historico.innerHTML = result + "<br>"
+  }
+  /*
+  hresult = result
+  historico.innerHTML = hprimeirovalor + "    " + "    " +  resultope + "    " + hsegundovalor + "<br>"
+  if ( result != "" ) {
+    historico.innerHTML += hprimeirovalor + "    " + "    " + resultope + "    " + hsegundovalor + "  =  " + hresult + "<br>"
+    resultope = ""
+    hprimeirovalor = result
+    hsegundovalor = ""
+    
+  }*/
+  
+    
+  console.log(hprimeirovalor)
+  console.log(hsegundovalor)
+  
+  console.log(resultope)
 }
